41376637bc9d6ca85c500b2e4e0ede34b304357e
Harvtronix <webmaster@harvtronix.com>
2023-10-04 17:10:22 -0400

    more scope analysis config

--- src/interpreter/program.ts
+++ src/interpreter/program.ts
@@ -1,19 +1,30 @@
 import { RootStatement } from '../parser/root-statement.js'
 import { ScopeAnalyzer } from './scope-analyzer.js'
 
+export interface Cell {
+  identifier: string
+  scope: Array<any>
+}
+
 export interface Memdef {
-  scope: Record<string, any>
+  cells: Record<string, Cell>
   scopeHierarchy: Array<string>
 }
 
 export class Program {
-  public readonly memory: Memdef = {
-    scope: {},
-    scopeHierarchy: []
-  }
+  public readonly scopeHierarchy: Array<string> = []
+  public readonly cells: Record<string, Cell> = {}
 
   public interpret(rootStatement: RootStatement) {
-    const scopeAnalyzer = new ScopeAnalyzer(this.memory)
+    const scopeAnalyzer = new ScopeAnalyzer(this)
     rootStatement.accept(scopeAnalyzer)
+  }
+
+  public hasIdentifier(identifier: string) {
+    const fullyQualifiedIdentifier = [...this.scopeHierarchy, identifier].join(
+      '.'
+    )
+
+    return fullyQualifiedIdentifier in this.cells
   }
 }
--- src/interpreter/scope-analyzer.ts
+++ src/interpreter/scope-analyzer.ts
@@ -2,37 +2,36 @@ import { DefineBody } from '../parser/blocks/define-body.js'
 import { Statement } from '../parser/statement.js'
 import { DefineStatement } from '../parser/statements/define-statement.js'
 import { InterpreterError } from './interpreter-error.js'
-import { Memdef } from './program.js'
+import { Program } from './program.js'
 import { StatementVisitor } from './statement-visitor.js'
 
 export class ScopeAnalyzer implements StatementVisitor {
-  constructor(private memory: Memdef) {}
+  constructor(private program: Program) {}
 
   visitComment(): Statement | void {
     // noop
   }
 
   visitDefineStatement(statement: DefineStatement): void {
-    if (statement.identifier in this.memory.scope) {
+    if (this.program.hasIdentifier(statement.identifier)) {
       throw new InterpreterError(`${statement.identifier} is already defined`)
     }
 
-    this.memory.scopeHierarchy.push(statement.identifier)
-    const scopeObj = this.memory.scopeHierarchy.reduce((prev, cur) => {
-      if (!(cur in prev)) {
-        prev[cur] = {}
-      }
-      return prev[cur]
-    }, this.memory.scope)
+    this.program.scopeHierarchy.push(statement.identifier)
+
+    this.program.cells[this.program.scopeHierarchy.join('.')] = {
+      identifier: statement.identifier,
+      scope: []
+    }
 
     statement.defineBody.accept(this)
 
-    this.memory.scopeHierarchy.pop()
+    this.program.scopeHierarchy.pop()
   }
 
   visitDefineBody(statement: DefineBody): void | Statement {
-    // statement.statements.forEach((statement) => {
-    //   statement.accept(this)
-    // })
+    statement.statements.forEach((statement) => {
+      statement.accept(this)
+    })
   }
 }
